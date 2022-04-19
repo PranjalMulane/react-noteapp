117cdfb33a29e5e91c6821b3cfe9a470aeea5ae7
Pranjal Mulane <pranjalsmulane@gmail.com>
2022-04-19 15:55:07 +0530

    user created

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,20 +1,25 @@
 const express = require('express');
-
-
-
-
 const router = express.Router();
+const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
 
+router.post('/',[
+    //validations
+    body('name','Enter valid name').isLength({min: 3}),
+    body('email','Enter valid Email').isEmail(),
+    body('password','Password must be of at least 8 characters').isLength({min : 8}),
 
-router.get('/',(req,res)=>{
-    // a = {
-    //     name : "pranjal",
-    //     role : "dev"
-    // }
-    // res.json(a);
-    console.log(req.body);
+],(req,res)=>{
+    
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
    
-     res.send(req.body);
+    // console.log(req.body);
+    const user = User(req.body);
+    user.save()
+    res.send(req.body);
 
 })
 
