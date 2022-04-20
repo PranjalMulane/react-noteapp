34def6e08b2b3fd4fb3438c790a342af8d11b043
Pranjal Mulane <pranjalsmulane@gmail.com>
2022-04-20 21:14:02 +0530

    password generated

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,34 +2,49 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+const { find, findOne } = require('../models/User');
+const bcrypt = require('bcryptjs');     //hash function  password 
 
-router.post('/',[
-    //validations
-    body('name','Enter valid name').isLength({min: 3}),
-    body('email','Enter valid Email').isEmail(),
-    body('password','Password must be of at least 8 characters').isLength({min : 8}),
+router.post('/createuser', [
+  //validations
+  body('name', 'Enter valid name').isLength({ min: 3 }),
+  body('email', 'Enter valid Email').isEmail(),
+  body('password', 'Password must be of at least 8 characters').isLength({ min: 8 }),
 
-],(req,res)=>{
-    
+], async (req, res) => {
+
+  //erroe handling
+  try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-   
     // console.log(req.body);
-    User.create({
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(400).json({ errors: "user already exists" });
+    }
+    //hash password generation
+    const salt = await bcrypt.genSaltSync(10);
+    const securedPass = await bcrypt.hash(req.body.password, salt)
+    //create user
+    user = await User.create({
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password,
-    }).then(user => res.json(user))
-    .catch(err => {console.log(err)
-        res.json({error: 'please enter unique email',
-        message : err.message
-      })
-    })  
-    
-
+      password: securedPass,
+    })
+    // .then(user => res.json(user))
+    // .catch(err => {console.log(err)
+    //     res.json({error: 'please enter unique email',
+    //     message : err.message
+    //   }) //////
+    // })  
+    res.json(user)
+  } catch (error) {
+    console.log(error.mesage);
+    res.status(500).json({ errors: "some error occured" });
+  }
 })
-
 module.exports = router
+
 
