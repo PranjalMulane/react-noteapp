599eb988cbd8bdf4aee68b7ee17215b87b8e08c2
Pranjal Mulane <pranjalsmulane@gmail.com>
2022-04-22 15:55:43 +0530

    login route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,7 +4,10 @@ const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
 const { find, findOne } = require('../models/User');
 const bcrypt = require('bcryptjs');     //hash function  password 
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = "youareawesome&";
 
+//ROUTE 1: CREATE USER
 router.post('/createuser', [
   //validations
   body('name', 'Enter valid name').isLength({ min: 3 }),
@@ -33,18 +36,75 @@ router.post('/createuser', [
       email: req.body.email,
       password: securedPass,
     })
+
+    const data = {
+      user: {
+        id: user.id
+      }
+    }
+    const authtoken = jwt.sign(data, JWT_SECRET)
+
     // .then(user => res.json(user))
     // .catch(err => {console.log(err)
     //     res.json({error: 'please enter unique email',
     //     message : err.message
     //   }) //////
     // })  
-    res.json(user)
+    // console.log(authtoken)
+    res.json({authtoken})
   } catch (error) {
     console.log(error.mesage);
     res.status(500).json({ errors: "some error occured" });
   }
 })
+
+
+//ROUTE 2 LOGIN 
+router.post('/login', [
+  body('email', 'Enter valid EMail').isEmail(),
+  body('password', 'Enter valid password').exists()
+], async (req, res) => {
+
+  const { email, password } = req.body;
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    
+    //user not exists
+    let user = await User.findOne({email});
+    if (!user) {
+      return res.status(400).json({ errors: "user not exists" });
+    }
+
+    const passwordComapre = await bcrypt.compare(password, user.password);
+    //password comaprasion
+    if(!passwordComapre){
+      return res.status(400).json({ errors: "Login credentials are invalid" });
+    }
+
+   const payload = {
+     user: {
+       id : user.id
+     }
+   }
+
+  const authtoken = jwt.sign(payload, JWT_SECRET)
+  res.json({authtoken})
+
+  } catch (error) {
+    console.log(error.mesage);
+    res.status(500).json({ errors: "some error occured" });
+  }
+
+
+})
+
+
+
+
+
 module.exports = router
 
 
