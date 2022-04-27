63b126ccadb0fc200f68d54c0e786461e7252116
Pranjal Mulane <pranjalsmulane@gmail.com>
2022-04-27 21:08:53 +0530

    Update and Delete notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const Note = require('../models/Note');
 const fetchuser = require('../middleware/fetchuser');
 const { body, validationResult } = require('express-validator');
+const { findByIdAndUpdate, findById } = require('../models/Note');
 
-// ROUTE 1: fetch all the notes
+// ROUTE 1: fetch all the notes    :   http://localhost:3000/api/notes/fetchallnotes
 router.get('/fetchallnotes', fetchuser, async (req, res) => {
 
     try {
@@ -16,7 +17,7 @@ router.get('/fetchallnotes', fetchuser, async (req, res) => {
     }
 })
 
-//Route 2 : create or add notes
+//Route 2 : create or add notes   : http://localhost:3000/api/notes/addnote
 router.post('/addnote', fetchuser, [
     body('title', 'Enter valid title').isLength({ min: 3 }),
     body('description', 'Enter valid description').isLength({ min: 3 }),
@@ -44,7 +45,7 @@ router.post('/addnote', fetchuser, [
 
 })
 
-//Route 3: Update note   
+//Route 3: Update note  //note ID  : http://localhost:3000/api/notes/updatenote/626944e14fe99a1763a35c79
 router.put('/updatenote/:id', fetchuser, async(req,res)=>{
     const {title, description, tag} = req.body;
 
@@ -54,22 +55,44 @@ router.put('/updatenote/:id', fetchuser, async(req,res)=>{
         if(description) {newNote.description = description};
         if(tag) {newNote.tag = tag};
 
-        let note = Note.findById(req.params.id)
+        let note = await Note.findById(req.params.id)
 
         if(!note) {return res.status(404).send("Note not found")}
 
-        
-        
+        if( note.user.toString() !== req.user.id){
+            return res.status(401).send("You are not allowed to update the note");
+        }
+
+        //update
+        note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true})
+
+        res.json({note})
     }catch (error) {
         console.log(error.mesage);
         res.status(500).json({ errors: "some error occured" });
     }
-
-
 })
 
+//Route 4: DELETE Note : http://localhost:3000/api/notes/deletenote/626944e14fe99a1763a35c79
+router.delete('/deletenote/:id', fetchuser , async(req,res) =>{
+    try{
 
+        let note =  await Note.findById(req.params.id)
 
+        if(!note){return res.status(401).send("Not Found")}
+
+        if( note.user.toString() !== req.user.id){
+            return res.status(401).send("You are not allowed to delete the note");
+        }
+
+        note = await Note.findByIdAndDelete(req.params.id)
+        res.json({"sucess":"Sucessfully deleted", note:note})
+
+    }catch (error) {
+        console.log(error.mesage);
+        res.status(500).json({ errors: "some error occured" });
+    }
+})
 
 
 module.exports = router
